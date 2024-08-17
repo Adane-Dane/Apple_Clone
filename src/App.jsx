5bffda582f225d83cbf36a9849f84565116fa5da
Adane-Dane <cocadane4@gmail.com>
2024-08-17 14:11:36 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import Header from "./Components/Header";
+import AlertSection from "./Components/AlertSection";
+import FirstSection from "./Components/FirstSection";
+import SecondSection from "./Components/SecondSection";
+import ThirdSection from "./Components/ThirdSection";
+import FourthSection from "./Components/FourthSection";
+import FifthSection1 from "./Components/FifthSection1";
+import Footer from "./Components/Footer";
+
+
+function App() {
+  return (
+    <>
+      <dir>
+        <Header />
+        <AlertSection />
+        <FirstSection />
+        <SecondSection />
+        <ThirdSection />
+        <FourthSection />
+        <FifthSection1 />
+        <Footer />
+      </dir>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/ColumonFooter.jsx
@@ -0,0 +1,46 @@
+import React, { Component } from 'react'
+
+export default class ColummonFooter extends Component {
+  render() {
+    return (
+      <div className="links-wrapper-1 col-sm-12 col-md">
+        <h3>Shop and Learn</h3>
+        <ul>
+          <li>
+            <a href="#">Mac</a>
+          </li>
+          <li>
+            <a href="#">iPad</a>
+          </li>
+          <li>
+            <a href="#">iPhone</a>
+          </li>
+          <li>
+            <a href="#">Watch</a>
+          </li>
+          <li>
+            <a href="#">TV</a>
+          </li>
+          <li>
+            <a href="#">Music</a>
+          </li>
+          <li>
+            <a href="#">AirPods</a>
+          </li>
+          <li>
+            <a href="#">HomePod</a>
+          </li>
+          <li>
+            <a href="#">iPod touch</a>
+          </li>
+          <li>
+            <a href="#">Accessories</a>
+          </li>
+          <li>
+            <a href="#">Gift Cards</a>
+          </li>
+        </ul>
+      </div>
+    );
+  }
+}
